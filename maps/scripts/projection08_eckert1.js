5ed66d9a8f964c966314d84ad21150d4b0bccfb7
Todd Fleeman <rtoddf@comcast.net>
2013-12-08 21:46:33 -0500

    eckert1 projection

---
+++ maps/scripts/projection08_eckert1.js
@@ -0,0 +1,22 @@
+// http://bl.ocks.org/mbostock/3734322
+
+var container_parent = $('.display') ,
+	chart_container = $('#example'),
+	margins = {top: 20, right: 20, bottom: 20, left: 20},
+	width = container_parent.width() - margins.left - margins.right,
+	height = width - margins.top - margins.bottom,
+	vis, vis_group, aspect
+
+var projection = d3.geo.eckert1()
+
+
+vis = d3.select('#example').append('svg')
+	.attr({
+		'width': width + margins.left + margins.right,
+		'height': height + margins.top + margins.bottom,
+		'preserveAspectRatio': 'xMinYMid',
+		'viewBox': '0 0 ' + (width + margins.left + margins.right) + ' ' + (height + margins.top + margins.bottom)
+	})
+
+vis_group = vis.append('g')
+aspect = chart_container.width() / chart_container.height()
