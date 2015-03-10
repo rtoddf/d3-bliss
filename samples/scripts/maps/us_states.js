784d6f4765644f2d2664a12e91b96463c2c48163
Todd Fleeman <rtoddf@comcast.net>
2015-03-09 23:04:15 -0400

    fixes and added new cat to state map

--- samples/scripts/maps/us_states.js
+++ samples/scripts/maps/us_states.js
@@ -14,7 +14,47 @@ vis_group = vis.append('g')
 aspect = chart_container.width() / chart_container.height()
 var names = {}
 var data_set
-var party_map = true
+var party_map = true,
+    same_sex_map = false,
+    age_of_consent_map = false
+
+// TODO: clean these functions up
+function state_party_fill(d){
+    var party = names[d.id].party
+    if(party == 'Republican'){
+        return '#e91d0e'
+    } else if(party == 'Democrat'){
+        return '#003264'
+    } else if(party == 'Split'){
+        return 'purple'
+    } else {
+        return 'white'
+    }
+}
+
+function state_data_fill(d, data_set){
+    var data_set = names[d.id][data_set]
+    if(data_set == 'Legal'){
+        return 'pink'
+    } else if(data_set == 'Illegal'){
+        return '#999'
+    } else {
+        return 'white'
+    }
+}
+
+function state_consent_fill(d, data_set){
+    var age_of_consent = names[d.id][data_set]
+    if(age_of_consent == 16){
+        return '#baba71'
+    } else if(age_of_consent == 17){
+        return '#909057'
+    } else if(age_of_consent == 18){
+        return '#5a5a36'
+    } else {
+        return 'white'
+    }
+}
 
 d3.tsv('../data/us-state-names.tsv', function(tsv){
     tsv.forEach(function(d, i){
@@ -22,48 +62,13 @@ d3.tsv('../data/us-state-names.tsv', function(tsv){
             'name': d.name,
             'code': d.code,
             'party': d.party,
-            'same_sex_marriage': d.same_sex_marriage
+            'same_sex_marriage': d.same_sex_marriage,
+            'age_of_consent': d.age_of_consent
         }
     })
 })
 
 d3.json('../data/us.json', function(error, topology){
-    // draw the US boundaries
-    // vis_group.append('path')
-    //     .datum(topojson.feature(topology, topology.objects.land))
-    //     .attr({
-    //         'd': path,
-    //         'fill': defaults.land.fill,
-    //         'stroke': defaults.land.stroke
-    //     })
-
-    console.log(topology)
-    console.log(names)
-
-    function state_party_fill(d){
-        var party = names[d.id].party
-        if(party == 'republican'){
-            return '#e91d0e'
-        } else if(party == 'democrat'){
-            return '#003264'
-        } else if(party == 'split'){
-            return 'purple'
-        } else {
-            return 'white'
-        }
-    }
-
-    function state_data_fill(d, data_set){
-        var data_set = names[d.id][data_set]
-        if(data_set == 'true'){
-            return 'pink'
-        } else if(data_set == 'false'){
-            return '#999'
-        } else {
-            return 'white'
-        }
-    }
-
     // draws the state shapes
     vis_group.selectAll('path')
             .data(topojson.feature(topology, topology.objects.states).features)
@@ -80,49 +85,63 @@ d3.json('../data/us.json', function(error, topology){
         .style({
             'cursor': 'pointer'
         })
-        .on('mouseover', function(d) {
-            d3.select('.tooltip')
-                .html( '<span>' + names[d.id]['code'] + ': ' + names[d.id]['name'] + '</span>' )
-                .style({
-                    'left': (d3.event.pageX) + 'px',
-                    'top': (d3.event.pageY - 28) + 'px'
-                })
-                .transition()
-                    .duration(500)
-                    .style({
-                        'opacity': 1
-                    }) 
+        .each(function(d) {
+            d3.select(this).on('mouseover', user_interaction)
+            d3.select(this).on('mouseout', user_interaction)
+        })
 
-            d3.select(this)
-                .transition()
-                    .duration(500)
-                    .attr({
-                        'fill': '#fff'
-                    })
-        })
-        .on('mouseout', function(d) {
-            d3.select('.tooltip')
-                .transition()
-                    .duration(200)
-                    .style('opacity', 0)
-            d3.select(this)
-                .transition()
-                    .duration(200)
-                    .attr({
-                        'fill': function(d){
-                            if(party_map){
-                                return state_party_fill(d)
-                            } else {
-                                return state_data_fill(d, data_set)
-                            }
-                        }
-                    })
-        })
+    function user_interaction(d){
+        var fill = function(){
+            // TODO: clean this up
+            if(party_map){
+                return state_party_fill(d)
+            } else if(age_of_consent_map){
+                return state_consent_fill(d, data_set)
+            } else if(same_sex_map) {
+                return state_data_fill(d, data_set)
+            }
+        }
+
+        var html = function(){
+            // TODO: clean this up
+            if(party_map){
+                return names[d.id]['party']
+            } else if(age_of_consent_map){
+                return names[d.id]['age_of_consent']
+            } else if(same_sex_map) {
+                return names[d.id]['same_sex_marriage']
+            }
+        }
+
+        var tooltip_opacity = d3.event.type == 'mouseover' ? 1 : 0
+        var fill_color = d3.event.type == 'mouseover' ? '#666' : fill()
+        
+        d3.select('.tooltip')
+            .html( '<span>' + names[d.id]['code'] + ': ' + html() + '</span>' )
+            .style({
+                'left': (d3.event.pageX) + 'px',
+                'top': (d3.event.pageY - 28) + 'px'
+            })
+            .transition()
+                .duration(500)
+                .style({
+                    'opacity': tooltip_opacity
+                }) 
+
+        d3.select(this)
+            .transition()
+                .duration(200)
+                .attr({
+                    'fill': fill_color
+                })
+    }
 
     function animate(data_set){
-        
-        if(data_set !== 'party'){
+        // TODO: clean this up
+        if(data_set == 'same_sex_marriage'){
             party_map = false
+            same_sex_map = true
+            age_of_consent_map = false
 
             d3.selectAll('.stats')
                 .transition()
@@ -133,8 +152,24 @@ d3.json('../data/us.json', function(error, topology){
                             return state_data_fill(d, data_set)
                         }
                     })
-        } else {
+        } else if(data_set == 'age_of_consent'){
+            party_map = false
+            same_sex_map = false
+            age_of_consent_map = true
+
+            d3.selectAll('.stats')
+                .transition()
+                    .duration(400)
+                    .ease('cubic')
+                    .attr({
+                        'fill': function(d){
+                            return state_consent_fill(d, data_set)
+                        }
+                    })
+        } else if(data_set == 'party'){
             party_map = true
+            same_sex_map = false
+            age_of_consent_map = false
 
             d3.selectAll('.stats')
                 .transition()
@@ -145,7 +180,7 @@ d3.json('../data/us.json', function(error, topology){
                             return state_party_fill(d)
                         }
                     })
-        }        
+        }
     }
 
     $('body').on('click', '[rel="program-share-modal"]', function( e ) {
