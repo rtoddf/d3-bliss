dcc5c890c4a93efda1414c55a0676ee780933b2f
Todd Fleeman <rtoddf@comcast.net>
2015-03-15 22:39:11 -0400

    finally got it working - now for a timer

--- samples/scripts/sortable_bar02.js
+++ samples/scripts/sortable_bar02.js
@@ -6,11 +6,29 @@ var container_parent = $('.display') ,
 	color = d3.scale.category20c(),
 	vis, vis_group, aspect
 
+var prefix = 'http://api.chartbeat.com/live/toppages/v3/'
+var options = '&limit=10&sortby=engaged_time'
+var key = 'cfa46a3950a4f0cda65b530b5cf05bf5'
+
+var news965_url = prefix + '?host=news965.com' + options + '&apikey=' + key
+var wsbradio_url = prefix + '?host=wsbradio.com' + options + '&apikey=' + key
+var wokv_url = prefix + '?host=wokv.com' + options + '&apikey=' + key
+var krmg_url = prefix + '?host=krmg.com' + options + '&apikey=' + key
+
+var data = []
+
 var x = d3.scale.linear()
 	.range([ 0, width ])
 
 var y = d3.scale.ordinal()
 	.rangeRoundBands([ 0, height], .1)
+
+queue()
+    .defer(d3.json, news965_url)
+    .defer(d3.json, wsbradio_url)
+    .defer(d3.json, wokv_url)
+    .defer(d3.json, krmg_url)
+    .await(ready);
 
 vis = d3.select('#chart').append('svg')
 	.attr({
@@ -27,18 +45,33 @@ vis_group = vis.append('g')
 
 aspect = chart_container.width() / chart_container.height()
 
-d3.json('data/us_census.json', function(error, data){
-	console.log(data)
+function ready(error, news965, wsbradio, wokv, krmg) {
 
-	x.domain([ 0, d3.max(data, function(d){
-		return parseInt(d.pop)
-	})])
+	news965.pages.forEach(function(d){
+        d.site = 'news965'
+        data.push(d)
+    })
+
+    wsbradio.pages.forEach(function(d){
+        d.site = 'wsbradio'
+        data.push(d)
+    })
+
+    wokv.pages.forEach(function(d){
+        d.site = 'wokv'
+        data.push(d)
+    })
+
+    krmg.pages.forEach(function(d){
+        d.site = 'krmg'
+        data.push(d)
+    })
 
     y.domain(data.sort(function(a, b) {
-        return d3.descending(a.pop, b.pop);
+        return d3.descending(a.stats.visits, b.stats.visits);
     })
-    .map(function(d) {
-        return d.placename;
+    .map(function(d, i) {
+        return d.site + d.title;
     }))
 
     var bar = vis_group.selectAll('.bar')
@@ -47,15 +80,39 @@ d3.json('data/us_census.json', function(error, data){
 			.attr({
 				'class': 'bar',
 				'transform': function(d, i) {
-					return 'translate(0,' + y(d.placename) + ')';
+					return 'translate(0,' + y(d.site + d.title) + ')';
 				}
 			})
 
 	bar.append('rect')
 		.attr({
+			'class': function(d){
+				return d.site
+			},
 			'height': y.rangeBand(),
-			'width': width,
-			'fill': 'orange'
+			'width': width
+		})
+
+	bar.append('text')
+		.attr({
+			'x': 10,
+			'y': y.rangeBand() / 2,
+			'dy': '.35em',
+			'fill': 'white'
+		})
+		.text(function(d){
+			return d.stats.visits
+		})
+
+	bar.append('text')
+		.attr({
+			'x': 50,
+			'y': y.rangeBand() / 2,
+			'dy': '.35em',
+			'fill': 'white'
+		})
+		.text(function(d){
+			return d.title
 		})
 
 	bar.append('text')
@@ -67,9 +124,9 @@ d3.json('data/us_census.json', function(error, data){
 			'fill': 'white'
 		})
 		.text(function(d){
-			console.log(d)
-			return d.pop
+			return d.stats.engaged_time.avg
 		})
+	
 
 	$('button').on('click', function(){
         var sort = $(this).data('sort')
@@ -79,37 +136,20 @@ d3.json('data/us_census.json', function(error, data){
     })
 
     function change() {
-        // Copy-on-write since tweens are evaluated after a delay.
         if(this.sort == 'desc'){
             var y0 = y.domain(data.sort(function(a, b) {
-                return d3.descending(a.pop, b.pop);
+                return d3.descending(a.stats.visits, b.stats.visits);
             })
             .map(function(d) {
-                return d.placename;
+                return d.site + d.title;
             }))
             .copy()
         } else if(this.sort == 'asc'){
             var y0 = y.domain(data.sort(function(a, b) {
-                return d3.ascending(a.pop, b.pop);
+                return d3.ascending(a.stats.visits, b.stats.visits);
             })
             .map(function(d) {
-                return d.placename;
-            }))
-            .copy()
-        } else if(this.sort == 'alpha_asc'){
-            var y0 = y.domain(data.sort(function(a, b) {
-                return d3.ascending(a.placename, b.placename);
-            })
-            .map(function(d) {
-                return d.placename;
-            }))
-            .copy()
-        } else{
-            var y0 = y.domain(data.sort(function(a, b) {
-                return d3.descending(a.placename, b.placename);
-            })
-            .map(function(d) {
-                return d.placename;
+                return d.site + d.title;
             }))
             .copy()
         }
@@ -126,18 +166,12 @@ d3.json('data/us_census.json', function(error, data){
             .ease('cubic')
             .attr({
             	'transform': function(d, i) {
-					return 'translate(0,' + y(d.placename) + ')';
+					return 'translate(0,' + y(d.site + d.title) + ')';
 				}
-			});
-
-        // this works
-        // transition.select('.y.axis')
-        //     .call(yAxis)
-        //         .selectAll('g')
-        //             .delay(delay);
+			})
     }
 
-})
+}
 
 
 
