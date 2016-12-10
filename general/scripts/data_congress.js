c4dddf4f213581f7b15360a326fb4172992992fb
Todd Fleeman <rtoddf@comcast.net>
2016-12-09 23:53:07 -0500

    cleaning up

--- general/scripts/data_congress.js
+++
@@ -1,5 +0,0 @@
-(function () {
-	d3.csv('../../../data/congress_members.csv', function(error, data){
-		console.log(data)
-	})
-})()
---
+++ reference/scripts/data_congress.js
@@ -0,0 +1,5 @@
+(function () {
+	d3.csv('data/congress_members.csv', function(error, data){
+		console.log(data)
+	})
+})()
